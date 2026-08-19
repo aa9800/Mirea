e48f069c2ebc06991e161d4e98661d17dd63c10f
aa9800 <a980000a@gmail.com>
2026-08-19 15:06:46 +0900

    과제 삭제: __임시테스트_삭제예정3__

--- assignments/test/01-임시테스트-삭제예정3/source/server.js
+++
@@ -1,4 +0,0 @@
-
-const http = require('http');
-const s = http.createServer((req,res)=>{res.writeHead(200,{'Content-Type':'text/html'});res.end('<html><body style="background:#7c5cff;color:#fff;font-size:40px;padding:40px;">FROM SAVED ASSIGNMENT SOURCE</body></html>');});
-s.listen(0, ()=>{ const p=s.address().port; console.log('Local: http://localhost:'+p+'/'); });
